5a7ff488d630542214b4f7aa24f359cf4ec6309a
Artur Barros <arturwbarros@gmail.com>
2023-01-25 12:01:59 -0300

    Primeiro commit

---
+++ src/shared/typeorm/migrations/1674598181683-CreateProducts.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateProducts1674598181683 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
